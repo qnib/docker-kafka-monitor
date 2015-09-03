c49e40fbfb5aff9ad403b499d998d912e3732a64
Christian Kniep <ckniep@gaikai.com>
2015-09-03 16:23:41 +0200

    bugfix scripts

---
+++ opt/qnib/kafka-offset-monitor/bin/start_monitor_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "X${ZK_DC}" != "X" ];then
+    sed -i'' -E "s#service \"zookeeper(@\w+)?\"#service \"zookeeper@${ZK_DC}\"#" /etc/consul-templates/zkui.conf.ctmpl
+fi
+
+consul-template -consul localhost:8500 -template "/etc/consul-templates/start_monitor.sh.ctmpl:/opt/qnib/kafka-offset-monitor/bin/start_monitor.sh: chmod +x /opt/qnib/kafka-offset-monitor/bin/start_monitor.sh; supervisorctl restart kafka-offset-monitor"
+
